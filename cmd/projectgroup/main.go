9311a1309f8204ddc9c6a60f492532eec1ba6a29
Coral <101916048+Coralnws@users.noreply.github.com>
2025-08-05 06:01:38 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/projectgroup/main.go
@@ -0,0 +1,23 @@
+// cmd/projectgroup/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"projectgroup/internal/projectgroup"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := projectgroup.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
